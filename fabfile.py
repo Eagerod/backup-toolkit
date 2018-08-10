6825dc984e50434aae9893e34e2cb19158224545
Aleem Haji <hajial@gmail.com>
2018-08-09 23:34:07 -0400

    Updated fabfile to support being run from any directory inside or outside the project.

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,26 @@
+import os
+
 from fabric.api import local, task, runs_once
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS_FILE = os.path.join(ROOT_DIR, 'requirements.txt')
 
 
 @task
 @runs_once
 def setup(quiet=False):
-    local('pip install {} -r requirements.txt'.format('--quiet' if quiet else ''))
+    local('pip install {} -r {}'.format('--quiet' if quiet else '', REQUIREMENTS_FILE))
 
 
 @task
 @runs_once
 def lint():
     setup(quiet=True)
-    local('flake8 .')
+    local('flake8 {}'.format(ROOT_DIR))
 
 
 @task
 @runs_once
 def install_globally():
-    local('pip install --upgrade .')
+    local('pip install --upgrade {}'.format(ROOT_DIR))
