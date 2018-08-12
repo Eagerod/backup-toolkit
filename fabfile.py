3d335005907c6177d1214167635d9c7e2f3a4fc7
Aleem Haji <hajial@gmail.com>
2018-08-12 14:29:30 -0400

    Added a function to the fabfile that raises an error if two games have the same source path configured. Can be used for validation while going through the process of adding games, since there tends to be a lot of copy-pasting, and potentially forgetting.

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
 import os
+import sys
+from collections import defaultdict
 
 from fabric.api import local, task, runs_once
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 REQUIREMENTS_FILE = os.path.join(ROOT_DIR, 'requirements.txt')
+SOURCE_DIR = os.path.join(ROOT_DIR, 'src')
 
 
 @task
@@ -24,3 +27,30 @@ def lint():
 @runs_once
 def install():
     local('pip install --upgrade -v {}'.format(ROOT_DIR))
+
+
+@task
+@runs_once
+def validate_config_paths(config_path=None):
+    sys.path.insert(0, SOURCE_DIR)
+
+    import yaml
+
+    from cli import DEFAULT_CONFIG_YAML_FILEPATH
+
+    if not config_path:
+        config_path = DEFAULT_CONFIG_YAML_FILEPATH
+
+    with open(config_path) as f:
+        config = yaml.load(f.read())
+
+    platforms = defaultdict(dict)
+    platform_keys = ('cygwin', 'windows', 'osx')
+    for game in config['games']:
+        for platform in platform_keys:
+            if platform in game:
+                game_path = game[platform]['local']
+                if game_path in platforms[platform]:
+                    raise Exception('{} has the same source directory as another game'.format(game['name']))
+                platforms[platform][game_path] = True
+           
