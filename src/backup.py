89fb8f5c9d6b9f191d78b8c2e61777b32beaf9fa
Aleem Haji <hajial@gmail.com>
2019-01-03 19:44:57 -0500

    Moved the metadata database class to its own module

--- src/backup.py
+++ src/backup.py
@@ -1,12 +1,11 @@
 import argparse
 import hashlib
-import json
 import os
-import sqlite3
 
 import requests
 
 from google_photos_api import GoogleCredentialsProvider, GooglePhotosAPI
+from metadata_database import MetadataDatabase
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -16,61 +15,6 @@ METADATA_DATABASE_FILENAME = 'metadata.sqlite3'
 GOOGLE_PHOTOS_READ_ONLY_SCOPES = [
     'photoslibrary.readonly'
 ]
-
-
-class MetadataDatabase(object):
-    """
-    Note -- This class is not something that's meant to be used in any kind of
-      production environment. It was entirely built for writing pretty, and not
-      necessarily very functional, or error-handled entrypoint code.
-    """
-    __db_path__ = None
-    __db__ = None
-    __cursor__ = None
-
-    @classmethod
-    def db(cls):
-        if cls.__db__:
-            return cls.__db__
-
-        cls.__db__ = sqlite3.connect(cls.__db_path__)
-        return cls.__db__
-
-    @classmethod
-    def cursor(cls):
-        if cls.__cursor__:
-            return cls.__cursor__
-
-        cls.__cursor__ = cls.db().cursor()
-        return cls.__cursor__
-
-    @classmethod
-    def init(cls, database_path):
-        cls.__db_path__ = database_path
-
-    @classmethod
-    def create(cls):
-        cls.cursor().execute("""
-            CREATE TABLE IF NOT EXISTS images (
-                id text,
-                md5 text,
-                filename text,
-                metadata text
-            );
-        """)
-        cls.db().commit()
-
-    @classmethod
-    def has_metadata(cls, media_item):
-        return cls.cursor().execute('SELECT id FROM images where id = ?', (media_item.id,)).fetchone() is not None
-
-    @classmethod
-    def add_item(cls, media_item, md5):
-        cls.cursor().execute("""
-            INSERT INTO images VALUES (?, ?, ?, ?)
-        """, (media_item.id, md5, media_item.filename, json.dumps(media_item.json))
-        )
-        cls.db().commit()
 
 
 def do_program():
