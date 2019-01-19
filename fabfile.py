fba1f3c4d2a29f87cca2157fd373f01550286a76
Aleem Haji <hajial@gmail.com>
2019-01-19 16:35:10 -0500

    moved game config validation into tests, rather than a fab command

--- fabfile.py
+++ fabfile.py
@@ -51,29 +51,3 @@ def coverage():
 @runs_once
 def install():
     local('pip install --upgrade -v {}'.format(ROOT_DIR))
-
-
-@task
-@runs_once
-def validate_config_paths(config_path=None):
-    sys.path.insert(0, SOURCE_DIR)
-
-    import yaml
-
-    from cli import DEFAULT_CONFIG_YAML_FILEPATH
-
-    if not config_path:
-        config_path = DEFAULT_CONFIG_YAML_FILEPATH
-
-    with open(config_path) as f:
-        config = yaml.load(f.read())
-
-    platforms = defaultdict(dict)
-    platform_keys = ('cygwin', 'windows', 'osx')
-    for game in config['games']:
-        for platform in platform_keys:
-            if platform in game:
-                game_path = game[platform]['local']
-                if game_path in platforms[platform]:
-                    raise Exception('{} has the same source directory as another game'.format(game['name']))
-                platforms[platform][game_path] = True
---
+++ tests/ext/games/test_default_configuration.py
@@ -0,0 +1,35 @@
+import os
+from collections import defaultdict
+from unittest import TestCase
+
+import yaml
+
+from backup import ext
+
+
+CONFIG_YAML_PATH = os.path.join(ext.__path__[0], 'games', 'config.yaml')
+PLATFORM_KEY_BLACKLIST = (
+    'name',
+    'aliases'
+)
+
+
+class ConfigYamlTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(ConfigYamlTestCase, cls).setUpClass()
+
+        cls.config_yaml = yaml.load(open(CONFIG_YAML_PATH).read())
+
+    def test_is_valid(self):
+        platforms = defaultdict(dict)
+        platform_keys = ('cygwin', 'windows', 'osx')
+        for game in self.config_yaml['games']:
+            for platform in game.iterkeys():
+                if platform in PLATFORM_KEY_BLACKLIST:
+                    continue
+
+                game_path = game[platform]['local']
+                if game_path in platforms[platform]:
+                    raise Exception('{} for {} has the same source directory as another game'.format(game['name'], platform))
+                platforms[platform][game_path] = True
